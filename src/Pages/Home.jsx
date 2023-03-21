57ca0590340faddf6f4b8c8be47c186625fdb581
jhaaman1 <ajha9608@gmail.com>
2023-03-21 12:08:16 +0530

    footer completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import "./Home.css";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Responsive, { Industry } from "../Components/Slicker/Industry";
 import Footer from "../Components/Footer/Footer";
+import Carousel from "../Components/Slicker/Industry";
+import CaptionCarousel from "../Components/Slicker/Industry";
+import Trial from "../Components/Slicker/Trial";
+import FooterOne from "../Components/Footer/FooterOne";
 
 const Home = () => {
   return (
@@ -17,9 +21,9 @@ const Home = () => {
         <Box
           display={["block", "block", "flex", "flex"]}
           justifyContent="space-between"
-          mt={[5,5,5,10]}
+          mt={[5, 5, 5, 10]}
         >
-          <Box border={"2px solid blue"} w={"45%"} className="hero-text">
+          <Box w={"45%"} className="hero-text">
             <Heading className="head">
               <span>Connecting</span>
               <br />
@@ -30,17 +34,16 @@ const Home = () => {
               skills, gain CV points, & get hired by your dream company.
             </Text>
           </Box>
-          <Box border={"2px solid green"} w="45%">
+          <Box w="45%">
             <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-              <GridItem w="100%" h="120px" className="grid-item"  />
-              <GridItem w="100%" h="120px" className="grid-item"  />
-              <GridItem w="100%" h="120px" className="grid-item"  />
-              <GridItem w="100%" h="120px" className="grid-item"  />
-              <GridItem w="100%" h="120px" className="grid-item"  />
-              <GridItem w="100%" h="120px" className="grid-item"  />
+              <GridItem w="100%" h="120px" className="grid-item" />
+              <GridItem w="100%" h="120px" className="grid-item" />
+              <GridItem w="100%" h="120px" className="grid-item" />
+              <GridItem w="100%" h="120px" className="grid-item" />
+              <GridItem w="100%" h="120px" className="grid-item" />
+              <GridItem w="100%" h="120px" className="grid-item" />
             </Grid>
           </Box>
-
         </Box>
 
         {/* industry vetran */}
@@ -48,13 +51,52 @@ const Home = () => {
         <hr />
 
         <Box className="industry">
-          {/* <Industry/> */}
+          {/* <Trial/> */}
+          {/* <CaptionCarousel/> */}
         </Box>
 
+        <Box mt={6}>
+          <Flex justifyContent={"space-between"}>
+            <Box className="three-box"></Box>
+            <Box className="three-box"></Box>
+            <Box className="three-box"></Box>
+          </Flex>
+        </Box>
+
+        <Box mt='5'> 
+          <Text className="explore">Explore Opportunities</Text>
+          <Text className="explore-p">Participate, Showcase Skills & Gain CV Points through online & offline opportunities of your interest & make your mark!</Text>
+          <Flex>
+            <Box className="four-box"></Box>
+            <Box className="four-box"></Box>
+            <Box className="four-box"></Box>
+            <Box className="four-box"></Box>
+          </Flex>
+        </Box>
+
+        <Box mt='5'> 
+          <Text className="explore">Explore Opportunities</Text>
+          <Text className="explore-p">Participate, Showcase Skills & Gain CV Points through online & offline opportunities of your interest & make your mark!</Text>
+          <Flex>
+            <Box className="four-box"></Box>
+            <Box className="four-box"></Box>
+            <Box className="four-box"></Box>
+            <Box className="four-box"></Box>
+          </Flex>
+        </Box>
+
+        {/* slider included Here */}
 
 
+        <Box mt={4}><Image src="https://cdn.unstop.com/uploads/images/unstop/mentorship1180x280.jpg?d=1180x280"/></Box>
+
+        {/* 1 slider need to put here */}
+
+
+        <Box mt={4}><Image src="https://d8it4huxumps7.cloudfront.net/uploads/images/63db3ac0bfbd4_download_app_new.png?d=2360x912"/></Box>
       </Box>
-        <Footer/>
+      {/* <Footer /> */}
+      <FooterOne/>
     </Box>
   );
 };
